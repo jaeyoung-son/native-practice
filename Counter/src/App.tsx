4c7b24524b801e3db14277999f786610cec6b8fe
손재영 <zkqkzl2@gmail.com>
2020-02-29 15:08:17 +0900

    카운터앱 만들기

---
+++ Counter/src/App.tsx
@@ -0,0 +1,67 @@
+import React, {Fragment} from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+import Counter from './Screens/Counter';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Styled from 'styled-components/native';
+
+const Container = Styled.View`
+  flex: 1;
+  background-color: #EEE;
+`;
+
+const App = () => {
+  return (
+    <Container>
+      <Counter title="Counter App" initValue={5} />
+    </Container>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
+
+export default App;
